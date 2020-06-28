b224068d8352cd6813c68399ebc7523dac601126
Sergey <sergei.werwolf@yandex.ru>
2020-06-29 02:03:51 +0300

    First commit

---
+++ app/src/main/java/com/main/mainapp/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.main.mainapp.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.main.mainapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
